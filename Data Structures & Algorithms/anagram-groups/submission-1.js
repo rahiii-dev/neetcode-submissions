a651395d8e2c390ecb9c14abe5239bf910adec38
Rahib K V <76057585+rahiii-dev@users.noreply.github.com>
2026-05-22 15:54:52 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = {};
+
+        for(let str of strs){
+            const count = new Array(26).fill(0);
+            
+            for(let c of str){
+                count[c.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+            }
+
+            const key = count.join();
+            
+            if(!map[key]){
+                map[key] = [];
+            }
+
+            map[key].push(str);
+        }
+
+        return Object.values(map);
+    }
+}
